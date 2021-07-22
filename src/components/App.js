3f5e5e0e39f240fa635c4eb1e662ed3c8d3da736
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-07-22 18:06:39 -0500

    docs: restructure files & remove react propaganda

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div>bonjour!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
